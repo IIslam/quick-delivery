f4d77445ff4ee2c0970c79030dcac240909efd76
Islam Ibrahim <i_gad_@hotmail.com>
2017-12-29 20:53:48 +0200

    remove push notifications plugin

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import * as PushNotifications from "nativescript-push-notifications";
+// import * as PushNotifications from "nativescript-push-notifications";
 
 @Component({
   selector: 'epw-app',
@@ -11,50 +11,50 @@ export class AppComponent {
   public constructor(translateService: TranslateService) {
     translateService.use('en');
 
-    let settings = {
-      senderID: "FIREBASE_SENDER_ID_HERE",
-      badge: true,
-      sound: true,
-      alert: true,
-      interactiveSettings: {
-        actions: [{
-          identifier: 'READ_IDENTIFIER',
-          title: 'Read',
-          activationMode: "foreground",
-          destructive: false,
-          authenticationRequired: true
-        }, {
-          identifier: 'CANCEL_IDENTIFIER',
-          title: 'Cancel',
-          activationMode: "foreground",
-          destructive: true,
-          authenticationRequired: true
-        }],
-        categories: [{
-          identifier: 'READ_CATEGORY',
-          actionsForDefaultContext: ['READ_IDENTIFIER', 'CANCEL_IDENTIFIER'],
-          actionsForMinimalContext: ['READ_IDENTIFIER', 'CANCEL_IDENTIFIER']
-        }]
-      },
-      notificationCallbackIOS: data => {
-        console.log("DATA: " + JSON.stringify(data));
-      },
-      notificationCallbackAndroid: (message, data
-        // , notification
-      ) => {
-        console.log("MESSAGE: " + JSON.stringify(message));
-        console.log("DATA: " + JSON.stringify(data));
-        // console.log("NOTIFICATION: " + JSON.stringify(notification));
-      }
-    };
+    // let settings = {
+    //   senderID: "FIREBASE_SENDER_ID_HERE",
+    //   badge: true,
+    //   sound: true,
+    //   alert: true,
+    //   interactiveSettings: {
+    //     actions: [{
+    //       identifier: 'READ_IDENTIFIER',
+    //       title: 'Read',
+    //       activationMode: "foreground",
+    //       destructive: false,
+    //       authenticationRequired: true
+    //     }, {
+    //       identifier: 'CANCEL_IDENTIFIER',
+    //       title: 'Cancel',
+    //       activationMode: "foreground",
+    //       destructive: true,
+    //       authenticationRequired: true
+    //     }],
+    //     categories: [{
+    //       identifier: 'READ_CATEGORY',
+    //       actionsForDefaultContext: ['READ_IDENTIFIER', 'CANCEL_IDENTIFIER'],
+    //       actionsForMinimalContext: ['READ_IDENTIFIER', 'CANCEL_IDENTIFIER']
+    //     }]
+    //   },
+    //   notificationCallbackIOS: data => {
+    //     console.log("DATA: " + JSON.stringify(data));
+    //   },
+    //   notificationCallbackAndroid: (message, data
+    //     // , notification
+    //   ) => {
+    //     console.log("MESSAGE: " + JSON.stringify(message));
+    //     console.log("DATA: " + JSON.stringify(data));
+    //     // console.log("NOTIFICATION: " + JSON.stringify(notification));
+    //   }
+    // };
 
 
-    PushNotifications.register(settings, data => {
-      console.log("REGISTRATION ID: " + JSON.stringify(data));
+    // PushNotifications.register(settings, data => {
+    //   console.log("REGISTRATION ID: " + JSON.stringify(data));
 
-      PushNotifications.onMessageReceived(settings.notificationCallbackAndroid);
-    }, error => {
-      console.log(error);
-    });
+    //   PushNotifications.onMessageReceived(settings.notificationCallbackAndroid);
+    // }, error => {
+    //   console.log(error);
+    // });
   }
 }
